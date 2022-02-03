788faf65af402d3b98a299f5dad439222fc3f335
ReshetovaOlga <reshetova_o_v@mail.ru>
2022-02-03 09:50:52 +0300

    first commit

---
+++ cmake-build-debug/CMakeFiles/MargaritaTheBest.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MargaritaTheBest.dir/main.c.obj"
+  "MargaritaTheBest.exe"
+  "MargaritaTheBest.exe.manifest"
+  "MargaritaTheBest.pdb"
+  "libMargaritaTheBest.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/MargaritaTheBest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
